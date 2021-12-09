d00fa4fc0b799805eb9cf497f6e37cb2e14554f3
Carson Farmer <carson.farmer@gmail.com>
2021-12-09 13:12:38 -0800

    feat: simplify contract + expect uuids + tests
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { v4 } from "uuid";
 const { BigNumber } = ethers;
 
 describe("Registry", function () {
@@ -12,14 +13,17 @@ describe("Registry", function () {
     // Manually call initialize because we are "deploying" the contract directly.
     await registry.initialize();
 
+    // Create a random uuid, strip out the -s and treat it like a hex value
+    const uuid = "0x" + v4().replace(/-/g, "");
+
     const tx = await registry
-      .connect(accounts[0]) // Use connect just to test things out
-      .safeMint(accounts[0].address);
+      .connect(accounts[4]) // Use connect to test that _anyone_ can mint
+      .mintOne(accounts[0].address, uuid);
     const receipt = await tx.wait();
     // Await for receipt and inspect events for token id etc.
     const [event] = receipt.events ?? [];
-    expect(event.args!.id).to.equal(BigNumber.from(0));
-    const balance = await registry.balanceOf(accounts[0].address, 0);
+    expect(event.args!.id).to.equal(BigNumber.from(uuid));
+    const balance = await registry.balanceOf(accounts[0].address, uuid);
     expect(1).to.equal(Number(balance.toString()));
   });
 });
