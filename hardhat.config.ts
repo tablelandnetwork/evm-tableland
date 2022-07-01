82c6835fe960c7d92508ed398351d3fceac1f531
Joe Wagner <njwjs722@gmail.com>
2022-07-01 10:20:54 -0600

    deployed proxy contract with new owner details

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -152,7 +152,7 @@ const config: HardhatUserConfig = {
     "ethereum-rinkeby-staging": "0x847645b7dAA32eFda757d3c10f1c82BFbB7b41D0", // deprecating: do not upgrade!
     "optimism-kovan-staging": "0x322F01e81c38B4211529f334864fA630F6aeA408",
     "optimism-kovan-staging-sqlite":
-      "0xEb64cA7F57729ac64a6eD3dc8F7bb03b570baeAD",
+      "0x2b51D72F210A96B6aB8EfB02dF8802363dcC83Dd",
     localhost: "",
   },
 };
