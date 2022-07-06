16bbe9ba6f6e7992b0cdca287c807a6b58095108
Sander Pick <sanderpick@gmail.com>
2022-07-06 10:49:32 -0700

    add local-tableland address to deployments lib
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- contracts/utils/TablelandDeployments.sol
+++ contracts/utils/TablelandDeployments.sol
@@ -21,6 +21,9 @@ library TablelandDeployments {
     // TablelandTables address on Polygon Mumbai.
     address internal constant POLYGON_MUMBAI =
         0x4b48841d4b32C4650E4ABc117A03FE8B51f38F68;
+    // TablelandTables address on for use with https://github.com/tablelandnetwork/local-tableland.
+    address internal constant LOCAL_TABLELAND =
+        0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512;
 
     /**
      * @dev Returns an interface to Tableland for the currently executing EVM chain.
@@ -36,6 +39,8 @@ library TablelandDeployments {
             return ITablelandTables(OPTIMISTIC_KOVAN);
         } else if (block.chainid == 80001) {
             return ITablelandTables(POLYGON_MUMBAI);
+        } else if (block.chainid == 31337) {
+            return ITablelandTables(LOCAL_TABLELAND);
         } else {
             revert ChainNotSupported(block.chainid);
         }
