a544b6b310b1ed5a31f1d147c488683e1e390034
Sander Pick <sanderpick@gmail.com>
2022-08-08 14:56:00 -0700

    add arbitrum, clean up tests
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- proxies.js
+++ proxies.js
@@ -2,16 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.proxies = void 0;
 exports.proxies = {
-    // tableland mainnet mainnets
+    // tableland testnet mainnets
     ethereum: "",
     optimism: "",
+    arbitrum: "",
     polygon: "",
     // tableland testnet testnets
     "ethereum-goerli": "0xDA8EA22d092307874f30A1F277D1388dca0BA97a",
-    "optimism-kovan": "0xf2C9Fc73884A9c6e6Db58778176Ab67989139D06",
+    "optimism-goerli": "",
+    "arbitrum-goerli": "",
     "polygon-mumbai": "0x4b48841d4b32C4650E4ABc117A03FE8B51f38F68",
     // tableland staging testnets
-    "optimism-kovan-staging": "0x7E57BaA6724c7742de6843094002c4e58FF6c7c3",
     "optimism-goerli-staging": "",
     localhost: "",
 };
