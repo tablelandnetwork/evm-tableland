7705deddb7f244e46df1791a0c389dfda1ca20f4
Sander Pick <sanderpick@gmail.com>
2022-08-09 13:10:27 -0700

    deploy to optimism-goerli, optimism-goerli-staging
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- proxies.js
+++ proxies.js
@@ -9,12 +9,12 @@ exports.proxies = {
     // tableland testnet testnets
     "ethereum-goerli": "0xDA8EA22d092307874f30A1F277D1388dca0BA97a",
     "optimism-kovan": "0xf2C9Fc73884A9c6e6Db58778176Ab67989139D06",
-    "optimism-goerli": "",
+    "optimism-goerli": "0xC72E8a7Be04f2469f8C2dB3F1BdF69A7D516aBbA",
     "arbitrum-goerli": "0x033f69e8d119205089Ab15D340F5b797732f646b",
     "polygon-mumbai": "0x4b48841d4b32C4650E4ABc117A03FE8B51f38F68",
     // tableland staging testnets
     "optimism-kovan-staging": "0x7E57BaA6724c7742de6843094002c4e58FF6c7c3",
-    "optimism-goerli-staging": "",
+    "optimism-goerli-staging": "0xfe79824f6E5894a3DD86908e637B7B4AF57eEE28",
     localhost: "",
 };
 //# sourceMappingURL=proxies.js.map
