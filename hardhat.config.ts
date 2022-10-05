8a79d45218a35bd608acc70920a15e36492cd3ed
Carson Farmer <carson.farmer@gmail.com>
2022-10-05 10:38:07 -0700

    chore: bye bye optimism kovan
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -43,7 +43,6 @@ const config: HardhatUserConfig = {
 
       // optimism
       optimisticEthereum: process.env.OPTIMISM_ETHERSCAN_API_KEY || "",
-      optimisticKovan: process.env.OPTIMISM_ETHERSCAN_API_KEY || "",
 
       // arbitrum
       arbitrumGoerli: process.env.ARBISCAN_API_KEY || "",
@@ -102,15 +101,6 @@ const config: HardhatUserConfig = {
           ? [process.env.ETHEREUM_GOERLI_PRIVATE_KEY]
           : [],
     },
-    "optimism-kovan": {
-      url: `https://opt-kovan.g.alchemy.com/v2/${
-        process.env.OPTIMISM_KOVAN_API_KEY ?? ""
-      }`,
-      accounts:
-        process.env.OPTIMISM_KOVAN_PRIVATE_KEY !== undefined
-          ? [process.env.OPTIMISM_KOVAN_PRIVATE_KEY]
-          : [],
-    },
     "optimism-goerli": {
       url: `https://opt-goerli.g.alchemy.com/v2/${
         process.env.OPTIMISM_GOERLI_API_KEY ?? ""
@@ -139,15 +129,6 @@ const config: HardhatUserConfig = {
           : [],
     },
     // devnets
-    "optimism-kovan-staging": {
-      url: `https://opt-kovan.g.alchemy.com/v2/${
-        process.env.OPTIMISM_KOVAN_STAGING_API_KEY ?? ""
-      }`,
-      accounts:
-        process.env.OPTIMISM_KOVAN_STAGING_PRIVATE_KEY !== undefined
-          ? [process.env.OPTIMISM_KOVAN_STAGING_PRIVATE_KEY]
-          : [],
-    },
     "optimism-goerli-staging": {
       url: `https://opt-goerli.g.alchemy.com/v2/${
         process.env.OPTIMISM_GOERLI_STAGING_API_KEY ?? ""
@@ -171,13 +152,10 @@ const config: HardhatUserConfig = {
     polygon: "https://testnet.tableland.network/chain/137/tables/",
     // testnets
     "ethereum-goerli": "https://testnet.tableland.network/chain/5/tables/",
-    "optimism-kovan": "https://testnet.tableland.network/chain/69/tables/",
     "optimism-goerli": "https://testnet.tableland.network/chain/420/tables/",
     "arbitrum-goerli": "https://testnet.tableland.network/chain/421613/tables/", // nitro testnet
     "polygon-mumbai": "https://testnet.tableland.network/chain/80001/tables/",
     // devnets
-    "optimism-kovan-staging":
-      "https://staging.tableland.network/chain/69/tables/",
     "optimism-goerli-staging":
       "https://staging.tableland.network/chain/420/tables/",
     localhost: "http://localhost:8080/chain/31337/tables/",
@@ -193,13 +171,11 @@ interface TablelandNetworkConfig {
 
   // testnets
   "ethereum-goerli": string;
-  "optimism-kovan": string;
   "optimism-goerli": string;
   "arbitrum-goerli": string;
   "polygon-mumbai": string;
 
   // devnets
-  "optimism-kovan-staging": string;
   "optimism-goerli-staging": string;
   localhost: string; // hardhat
 }
