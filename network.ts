433b2a6b2b098828f36dc2553d2b2ed41b6e65dd
Sander Pick <sanderpick@gmail.com>
2022-10-09 14:26:45 -0700

    deploy to arbitrum
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- network.ts
+++ network.ts
@@ -18,7 +18,7 @@ export interface TablelandNetworkConfig {
 export const proxies: TablelandNetworkConfig = {
   ethereum: "0x012969f7e3439a9B04025b5a049EB9BAD82A8C12",
   optimism: "0xfad44BF5B843dE943a09D4f3E84949A11d3aa3e6",
-  arbitrum: "",
+  arbitrum: "0x9aBd75E8640871A5a20d3B4eE6330a04c962aFfd",
   polygon: "0x5c4e6A9e5C1e1BF445A062006faF19EA6c49aFeA",
   "ethereum-goerli": "0xDA8EA22d092307874f30A1F277D1388dca0BA97a",
   "optimism-goerli": "0xC72E8a7Be04f2469f8C2dB3F1BdF69A7D516aBbA",
