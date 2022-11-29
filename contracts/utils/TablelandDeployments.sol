b8ef26d581b54a9dbebb144e9c0c8a9f64791a9c
dtbuchholz <dbuchholz30@gmail.com>
2022-11-29 11:18:10 -0800

    feat: add arbitrum mainnet config

--- contracts/utils/TablelandDeployments.sol
+++ contracts/utils/TablelandDeployments.sol
@@ -18,6 +18,9 @@ library TablelandDeployments {
     // TablelandTables address on Optimism.
     address internal constant OPTIMISTIC_ETHEREUM =
         0xfad44BF5B843dE943a09D4f3E84949A11d3aa3e6;
+    // TablelandTables address on Arbitrum.
+    address internal constant ARBITRUM =
+        0x9aBd75E8640871A5a20d3B4eE6330a04c962aFfd;
     // TablelandTables address on Polygon.
     address internal constant POLYGON =
         0x5c4e6A9e5C1e1BF445A062006faF19EA6c49aFeA;
