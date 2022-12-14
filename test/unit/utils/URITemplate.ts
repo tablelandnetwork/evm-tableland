77dba897b2c3a8cbf15d9904af117de2a5907eee
Dan Buchholz <dbuchholz30@gmail.com>
2022-12-13 16:53:25 -0800

    test: add tests for 'URITemplate.sol'

--- test/unit/utils/URITemplate.ts
+++ test/unit/utils/URITemplate.ts
@@ -2,13 +2,12 @@ import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers } from "hardhat";
-import { TestURITemplate, TestURITemplateLib } from "../../../typechain-types";
+import { TestURITemplate } from "../../../typechain-types";
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 let contract: TestURITemplate;
-let lib: TestURITemplateLib;
 let accounts: SignerWithAddress[];
 
 describe("URITemplate", function () {
@@ -20,171 +19,74 @@ describe("URITemplate", function () {
     accounts = await ethers.getSigners();
   });
 
-  it("Should not create invalid template URI", async function () {
-    // Check that empty strings do not set a new URI
-    await expect(contract.setURITemplateSingle("")).to.be.rejectedWith(
-      "InvalidTemplate()"
-    );
-    await expect(contract.setURITemplateMultiple("")).to.be.rejectedWith(
-      "InvalidTemplate()"
-    );
-  });
+  describe("_setURITemplate", function () {
+    it("Should return empty string for unset URI", async function () {
+      // Mint an NFT
+      const minter = accounts[1];
+      const tx = await contract.connect(minter).mint();
+      const receipt = await tx.wait();
+      const [event] = receipt.events ?? [];
+      const tokenId = event.args?.tokenId;
+      // Check that empty strings are returned
+      expect(await contract.tokenURI(tokenId)).to.equal(``);
+    });
 
-  it("Should return empty strings for unset token URI", async function () {
-    // Mint an NFT
-    const minter = accounts[1];
-    const tx = await contract.connect(minter).mint();
-    const receipt = await tx.wait();
-    const [event] = receipt.events ?? [];
-    const tokenId = event.args?.tokenId;
-    // Check that empty strings are returned
-    expect(await contract.tokenURI(tokenId)).to.equal(``);
-  });
+    it("Should set, update, and get URI with no '{id}' substring", async function () {
+      // Set token URI
+      let uriString = "https://foo.xyz/";
+      await contract.setURITemplate([uriString]);
+      // Mint an NFT
+      const minter = accounts[1];
+      const tx = await contract.connect(minter).mint();
+      const receipt = await tx.wait();
+      const [event] = receipt.events ?? [];
+      const tokenId = event.args?.tokenId;
+      // Get the token URI, which uses the template URI
+      expect(await contract.tokenURI(tokenId)).to.equal(uriString + tokenId);
+      // Set a new token URI
+      uriString = "https://foo.xyz/asdf/";
+      await contract.setURITemplate([uriString]);
+      let f = await contract.tokenURI(tokenId);
+      console.log(f);
+      expect(await contract.tokenURI(tokenId)).to.equal(uriString + tokenId);
+    });
 
-  it("Should create template URI with single 'id'", async function () {
-    // Set token URI
-    await contract.setURITemplateSingle("https://foo.xyz/{id}/bar");
-    // Mint an NFT
-    const minter = accounts[1];
-    const tx = await contract.connect(minter).mint();
-    const receipt = await tx.wait();
-    const [event] = receipt.events ?? [];
-    const tokenId = event.args?.tokenId;
-    expect(await contract.tokenURI(tokenId)).to.equal(
-      `https://foo.xyz/${tokenId}/bar`
-    );
+    it("Should set, update, and get URI with a single '{id}' substring", async function () {
+      // Set token URI
+      let uriString = "https://foo.xyz/{id}/bar";
+      const uri = uriString.split("{id}");
+      await contract.setURITemplate(uri);
+      // Mint an NFT
+      const minter = accounts[1];
+      const tx = await contract.connect(minter).mint();
+      const receipt = await tx.wait();
+      const [event] = receipt.events ?? [];
+      const tokenId = event.args?.tokenId;
+      // Get the token URI, which uses the template URI
+      expect(await contract.tokenURI(tokenId)).to.equal(uri.join(tokenId));
+      // Set a new token URI
+      uriString = "https://foo.xyz/{id}/asdf";
+      await contract.setURITemplate(uri);
+      expect(await contract.tokenURI(tokenId)).to.equal(uri.join(tokenId));
+    });
 
-    await contract.setURITemplateMultiple("https://foo.xyz/{id}/asdf");
-    expect(await contract.tokenURI(tokenId)).to.equal(
-      `https://foo.xyz/${tokenId}/asdf`
-    );
-  });
-
-  it("Should fail to create URI with multiple 'id' using the 'single' method", async function () {
-    // Set token URI
-    await contract.setURITemplateSingle("https://foo.xyz/{id}/bar={id}/foo");
-    // Mint an NFT
-    const minter = accounts[1];
-    const tx = await contract.connect(minter).mint();
-    const receipt = await tx.wait();
-    const [event] = receipt.events ?? [];
-    const tokenId = event.args?.tokenId;
-    // Check the token URI is the incorrect format
-    expect(await contract.tokenURI(tokenId)).to.equal(
-      `https://foo.xyz/${tokenId}/bar={id}/foo`
-    );
-  });
-
-  it("Should create template URI with multiple 'id'", async function () {
-    // Set token URI
-    await contract.setURITemplateMultiple("https://foo.xyz/{id}/bar={id}/foo");
-    // Mint an NFT
-    const minter = accounts[1];
-    const tx = await contract.connect(minter).mint();
-    const receipt = await tx.wait();
-    const [event] = receipt.events ?? [];
-    const tokenId = event.args?.tokenId;
-    // Check the token URI is the correct format
-    expect(await contract.tokenURI(tokenId)).to.equal(
-      `https://foo.xyz/${tokenId}/bar=${tokenId}/foo`
-    );
-    // Set the URI again, with a longer string and another 'id' substring
-    await contract.setURITemplateMultiple(
-      `https://foo.xyz/{id}/bar={id}/foo={id}/bar`
-    );
-    // Check the token URI is the correct format
-    expect(await contract.tokenURI(tokenId)).to.equal(
-      `https://foo.xyz/${tokenId}/bar=${tokenId}/foo=${tokenId}/bar`
-    );
-  });
-});
-
-describe("URITemplateLib", function () {
-  // Deploy the contract and get signers
-  beforeEach(async function () {
-    const UriTemplateLib = await ethers.getContractFactory(
-      "TestURITemplateLib"
-    );
-    lib = (await UriTemplateLib.deploy()) as TestURITemplateLib;
-    await contract.deployed();
-    accounts = await ethers.getSigners();
-  });
-
-  it("Should not create invalid template URI", async function () {
-    // Check that empty strings do not set a new URI
-    await expect(lib.setURITemplateSingle("")).to.be.rejectedWith(
-      "InvalidTemplate()"
-    );
-    await expect(lib.setURITemplateMultiple("")).to.be.rejectedWith(
-      "InvalidTemplate()"
-    );
-  });
-
-  it("Should return empty strings for unset token URI", async function () {
-    // Mint an NFT
-    const minter = accounts[1];
-    const tx = await lib.connect(minter).mint();
-    const receipt = await tx.wait();
-    const [event] = receipt.events ?? [];
-    const tokenId = event.args?.tokenId;
-    // Check that empty strings are returned
-    expect(await lib.tokenURI(tokenId)).to.equal(``);
-  });
-
-  it("Should create template URI with single 'id'", async function () {
-    // Set token URI
-    await lib.setURITemplateSingle("https://foo.xyz/{id}/bar");
-    // Mint an NFT
-    const minter = accounts[1];
-    const tx = await lib.connect(minter).mint();
-    const receipt = await tx.wait();
-    const [event] = receipt.events ?? [];
-    const tokenId = event.args?.tokenId;
-    expect(await lib.tokenURI(tokenId)).to.equal(
-      `https://foo.xyz/${tokenId}/bar`
-    );
-    // Set the URI again, with a longer string and another 'id' substring
-    await lib.setURITemplateMultiple("https://foo.xyz/{id}/asdf");
-    // Check the token URI is the correct format
-    expect(await lib.tokenURI(tokenId)).to.equal(
-      `https://foo.xyz/${tokenId}/asdf`
-    );
-  });
-
-  it("Should fail to create URI with multiple 'id' using the 'single' method", async function () {
-    // Set token URI
-    await contract.setURITemplateSingle("https://foo.xyz/{id}/bar={id}/foo");
-    // Mint an NFT
-    const minter = accounts[1];
-    const tx = await contract.connect(minter).mint();
-    const receipt = await tx.wait();
-    const [event] = receipt.events ?? [];
-    const tokenId = event.args?.tokenId;
-    // Check the token URI is the incorrect format
-    expect(await contract.tokenURI(tokenId)).to.equal(
-      `https://foo.xyz/${tokenId}/bar={id}/foo`
-    );
-  });
-
-  it("Should create template URI with multiple 'id'", async function () {
-    // Set token URI
-    await lib.setURITemplateMultiple("https://foo.xyz/{id}/bar={id}/foo");
-    // Mint an NFT
-    const minter = accounts[1];
-    const tx = await lib.connect(minter).mint();
-    const receipt = await tx.wait();
-    const [event] = receipt.events ?? [];
-    const tokenId = event.args?.tokenId;
-    expect(await lib.tokenURI(tokenId)).to.equal(
-      `https://foo.xyz/${tokenId}/bar=${tokenId}/foo`
-    );
-    // Set the URI again, with a longer string and another 'id' substring
-    await lib.setURITemplateMultiple(
-      `https://foo.xyz/{id}/bar={id}/foo={id}/bar`
-    );
-    // Check the token URI is the correct format
-    expect(await lib.tokenURI(tokenId)).to.equal(
-      `https://foo.xyz/${tokenId}/bar=${tokenId}/foo=${tokenId}/bar`
-    );
+    it("Should set, update, and get URI with multiple '{id}' substrings", async function () {
+      // Set token URI
+      let uriString = "https://foo.xyz/{id}/bar/{id}";
+      const uri = uriString.split("{id}");
+      await contract.setURITemplate(uri);
+      // Mint an NFT
+      const minter = accounts[1];
+      const tx = await contract.connect(minter).mint();
+      const receipt = await tx.wait();
+      const [event] = receipt.events ?? [];
+      const tokenId = event.args?.tokenId;
+      // Get the token URI, which uses the template URI
+      expect(await contract.tokenURI(tokenId)).to.equal(uri.join(tokenId));
+      // Set a new token URI
+      uriString = "https://foo.xyz/{id}/asdf/{id}";
+      await contract.setURITemplate(uri);
+      expect(await contract.tokenURI(tokenId)).to.equal(uri.join(tokenId));
+    });
   });
 });
