5ee5af2475b336ab37b06becee41cf3280c443f9
Sander Pick <sanderpick@gmail.com>
2023-03-24 14:58:47 -0700

    deployments: match chain names to new style, add arbitrum one and nova handling
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- contracts/utils/TablelandDeployments.sol
+++ contracts/utils/TablelandDeployments.sol
@@ -15,27 +15,32 @@ library TablelandDeployments {
     // TablelandTables address on Ethereum.
     address internal constant MAINNET =
         0x012969f7e3439a9B04025b5a049EB9BAD82A8C12;
+    // TablelandTables address on Ethereum.
+    address internal constant HOMESTEAD = MAINNET;
     // TablelandTables address on Optimism.
-    address internal constant OPTIMISTIC_ETHEREUM =
+    address internal constant OPTIMISM =
         0xfad44BF5B843dE943a09D4f3E84949A11d3aa3e6;
-    // TablelandTables address on Arbitrum.
+    // TablelandTables address on Arbitrum One.
     address internal constant ARBITRUM =
         0x9aBd75E8640871A5a20d3B4eE6330a04c962aFfd;
+    // TablelandTables address on Arbitrum Nova.
+    address internal constant ARBITRUM_NOVA =
+        0x1A22854c5b1642760a827f20137a67930AE108d2;
     // TablelandTables address on Polygon.
-    address internal constant POLYGON =
+    address internal constant MATIC =
         0x5c4e6A9e5C1e1BF445A062006faF19EA6c49aFeA;
 
     // TablelandTables address on Ethereum Goerli.
     address internal constant GOERLI =
         0xDA8EA22d092307874f30A1F277D1388dca0BA97a;
     // TablelandTables address on Optimism Goerli.
-    address internal constant OPTIMISTIC_GOERLI =
+    address internal constant OPTIMISM_GOERLI =
         0xC72E8a7Be04f2469f8C2dB3F1BdF69A7D516aBbA;
     // TablelandTables address on Arbitrum Goerli.
     address internal constant ARBITRUM_GOERLI =
         0x033f69e8d119205089Ab15D340F5b797732f646b;
     // TablelandTables address on Polygon Mumbai.
-    address internal constant POLYGON_MUMBAI =
+    address internal constant MATICMUM =
         0x4b48841d4b32C4650E4ABc117A03FE8B51f38F68;
 
     // TablelandTables address on for use with https://github.com/tablelandnetwork/local-tableland.
@@ -55,17 +60,21 @@ library TablelandDeployments {
         if (block.chainid == 1) {
             return ITablelandTables(MAINNET);
         } else if (block.chainid == 10) {
-            return ITablelandTables(OPTIMISTIC_ETHEREUM);
+            return ITablelandTables(OPTIMISM);
+        } else if (block.chainid == 42161) {
+            return ITablelandTables(ARBITRUM);
+        } else if (block.chainid == 42170) {
+            return ITablelandTables(ARBITRUM_NOVA);
         } else if (block.chainid == 137) {
-            return ITablelandTables(POLYGON);
+            return ITablelandTables(MATIC);
         } else if (block.chainid == 5) {
             return ITablelandTables(GOERLI);
         } else if (block.chainid == 420) {
-            return ITablelandTables(OPTIMISTIC_GOERLI);
+            return ITablelandTables(OPTIMISM_GOERLI);
         } else if (block.chainid == 421613) {
             return ITablelandTables(ARBITRUM_GOERLI);
         } else if (block.chainid == 80001) {
-            return ITablelandTables(POLYGON_MUMBAI);
+            return ITablelandTables(MATICMUM);
         } else if (block.chainid == 31337) {
             return ITablelandTables(LOCAL_TABLELAND);
         } else {
